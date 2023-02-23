4fc997675c544f06a7a42affeb4894559bb5bf0d
Jafar99 <jafaralihajaro@gmail.com>
2023-02-23 13:36:47 +0530

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+
 import './App.css';
+import kra from '../src/kra.png';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className='marquee'>
+          
+              <img src={kra} alt="" />
+           
+          
+            <marquee className="marq" >
+            <div className='tab'><span className='text'>Gold 24KT - RS.5677.20 Per Gm</span></div>
+            <div  className='tab2'><span className='text'>Gold 24KT - RS.5283.70 Per Gm</span></div>
+            <div  className='tab'><span className='text'>Gold 24KT - RS.4496.80 Per Gm</span></div>
+            <div  className='tab2'><spa className='text'>Pune Sliver - Rs.6680 per Gm</spa></div>
+            </marquee>
+           
+        </div>
     </div>
   );
 }
